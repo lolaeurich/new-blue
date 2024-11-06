330b4cb322212c3595e7100e69e3c64dc8517238
Geferson Lopes <gefersonjefreey@gmail.com>
2024-11-05 23:13:18 -0300

    feat: update carrousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useRef } from "react";
 import "./style.css";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const Carousel = ({ items }) => {
   const carouselRef = useRef(null);
@@ -16,18 +18,34 @@ const Carousel = ({ items }) => {
   };
 
   return (
-    <div className="carousel-container">
-      <button className="carousel-button left" onClick={() => handleScroll("left")}>{"<"}</button>
-      <div className="carousel" ref={carouselRef}>
-        {items.map((item) => (
-          <div key={item.id} className="carousel-item">
-            <img src={item.img} alt={item.name} className="carousel-image" />
-            <p className="carousel-name">{item.name}</p>
-          </div>
-        ))}
+    <section>
+      <header>
+        <p>Projetos</p>
+        <GiSettingsKnobs style={{ rotate: "90deg" }} />
+      </header>
+      <div className="carousel-container">
+        <button
+          className="carousel-button left"
+          onClick={() => handleScroll("left")}
+        >
+          <IoIosArrowBack />
+        </button>
+        <div className="carousel" ref={carouselRef}>
+          {items.map((item) => (
+            <div key={item.id} className="carousel-item">
+              <img src={item.img} alt={item.name} className="carousel-image" />
+              <p className="carousel-name">{item.name}</p>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-button right"
+          onClick={() => handleScroll("right")}
+        >
+          <IoIosArrowForward />
+        </button>
       </div>
-      <button className="carousel-button right" onClick={() => handleScroll("right")}>{">"}</button>
-    </div>
+    </section>
   );
 };
 
