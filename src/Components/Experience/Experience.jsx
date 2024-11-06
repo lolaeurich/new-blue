a834f8280e115f333f4025314f22ec1bdb1de13b
Geferson Lopes <gefersonjefreey@gmail.com>
2024-11-06 03:17:15 -0300

    feat: update xp

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
 import icon1 from "../../assets/Experience/icon1.png";
 import icon2 from "../../assets/Experience/icon2.png";
@@ -8,24 +8,49 @@ import icon5 from "../../assets/Experience/icon5.png";
 import icon6 from "../../assets/Experience/icon6.png";
 
 const Experience = () => {
-
-    return (
-        <div className='experience'>
-            <div className='experience-text'>
-                <h2>Mais de 16 anos de experiência no mercado imobiliário</h2>
-                <p>O que nos move é a busca insaciável por inovação e a certeza da excelência.</p>
-
-                <ul>
-                    <li><img alt='' src={icon1}/>Solidez</li>
-                    <li><img alt='' src={icon2}/>Diligência profunda</li>
-                    <li><img alt='' src={icon3}/>Histórico impecável</li>
-                    <li><img alt='' src={icon4}/>Viabilidade financeira</li>
-                    <li><img alt='' src={icon5}/>Controladoria minuciosa</li>
-                    <li><img alt='' src={icon6}/>Equipe de alta performance</li>
-                </ul>
-            </div>
+  return (
+    <div className="experience">
+      <div className="experience-text">
+        <div className="container">
+          <h2>
+            <strong>Mais de 16 anos de experiência</strong> no mercado
+            imobiliário
+          </h2>
+          <p>
+            O que nos move é a busca insaciável por inovação e a certeza da
+            excelência.
+          </p>
         </div>
-    );
-}
+
+        <ul>
+          <li>
+            <img alt="" src={icon1} />
+            Solidez
+          </li>
+          <li>
+            <img alt="" src={icon2} />
+            Diligência profunda
+          </li>
+          <li>
+            <img alt="" src={icon3} />
+            Histórico impecável
+          </li>
+          <li>
+            <img alt="" src={icon4} />
+            Viabilidade financeira
+          </li>
+          <li>
+            <img alt="" src={icon5} />
+            Controladoria minuciosa
+          </li>
+          <li>
+            <img alt="" src={icon6} />
+            Equipe de alta performance
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Experience;
